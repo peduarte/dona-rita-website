d036281fd421ba02e41e151cfe9de0e832ffe5b5
Pedro Duarte <contact@peduarte.com>
2018-01-29 18:10:50 +0000

    🔧 Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -28,7 +28,7 @@ module.exports = {
 				path: `${__dirname}/src/pages/`
 			}
 		},
-		'gatsby-plugin-netlify',
-		'gatsby-plugin-netlify-cms'
+		'gatsby-plugin-netlify-cms',
+		'gatsby-plugin-netlify'	
 	]
 };
